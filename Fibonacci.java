ed1edc857a5fc17f0bb8ac70eb33e6afe73b6d91
Navil Hernandez <navilhrnndz@gmail.com>
2024-01-21 03:29:06 -0500

    Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,11 @@
+public class Fibonacci {
+
+    // Recursive method to calculate the nth term in the Fibonacci sequence
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
