78d43bd4af55fb80642212affb9c3c5f2c1728c9
Navil Hernandez <navilhrnndz@gmail.com>
2024-01-21 23:24:34 -0500

    Add Javadoc comments to Fibonacci class

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,14 @@
+/**
+ * The Fibonacci class provides a method to calculate the nth term in the Fibonacci sequence.
+ */
 public class Fibonacci {
 
-    // Recursive method to calculate the nth term in the Fibonacci sequence
+    /**
+     * Recursive method to calculate the nth term in the Fibonacci sequence.
+     *
+     * @param n The position of the term to be calculated (non-negative integer).
+     * @return The value of the nth term in the Fibonacci sequence.
+     */
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
